f835a4e8fffd47cca7bb41069c2756b3fe5188ae
ngxuanbminh <163402925+ngxuanbminh@users.noreply.github.com>
2025-03-08 13:33:18 +0100

    add hardcoded JobView

--- vue-learn-crash/src/router/index.js
+++ vue-learn-crash/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/view/HomeView.vue'
 import JobsView from '@/view/JobsView.vue'
+import NotFoundView from '@/view/NotFoundView.vue'
+import JobView from '@/view/JobView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/jobs',
       name: 'jobs',
       component: JobsView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView,
+    },
+    {
+      path: '/jobs/:id',
+      name: 'job',
+      component: JobView,
     },
   ],
 })

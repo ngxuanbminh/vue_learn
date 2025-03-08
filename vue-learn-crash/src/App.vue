a7c13a2909e23986a1d510c2d701adbd54b70790
ngxuanbminh <163402925+ngxuanbminh@users.noreply.github.com>
2025-03-08 12:21:52 +0100

    home page done

--- vue-learn-crash/src/App.vue
+++ vue-learn-crash/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 import HeroItem from './components/HeroItem.vue'
+import HomeCards from './components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
 </script>
 
 <template>
   <NavBar />
   <HeroItem />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
